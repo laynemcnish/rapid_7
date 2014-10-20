b01cb738b6e9e1da1fdcfbc03e0ba405d9344c12
Layne McNish <lmcnish14@gmail.com>
2014-10-20 13:34:45 -0600

    added new methods to find the duplicated values and more tests

--- lib/rapid7.rb
+++ lib/rapid7.rb
@@ -8,4 +8,24 @@ class Rapid7
 
   end
 
+  def count_duplicates(input_array)
+    items_count = Hash.new(0)
+    input_array.each do |item|
+      items_count[item] += 1
+    end
+    items_count
+  end
+
+  def return_duplicated_values(input_array)
+    duplicated_values =[]
+    duplicate_count = count_duplicates(input_array)
+
+    duplicate_count.each do |key, value|
+      if value > 1
+        duplicated_values.push(key)
+      end
+    end
+    duplicated_values
+  end
+
 end
--- spec/rapid7_spec.rb
+++ spec/rapid7_spec.rb
@@ -21,4 +21,21 @@ describe Rapid7 do
   it "original function should return the values with duplicates when given an unordered array with duplicated values" do
     expect(@array.function([3,1,2,3,4,5,1,6,2].sort)).to eq([1,2,3])
   end
+
+  it "new method should return an empty array if given an empty array to start" do
+    expect(@array.return_duplicated_values([]).sort).to eq([])
+  end
+
+  it "new method should return an empty array when given an array that has only unique values" do
+    expect(@array.return_duplicated_values([]).sort).to eq([])
+  end
+
+  it "new method should return the values with duplicates when given an array with duplicated values" do
+    expect(@array.return_duplicated_values([1,1,2,2,3,3,4,5,6]).sort).to eq([1,2,3])
+  end
+
+  it "new method should return the values with duplicates when given an unordered array" do
+    expect(@array.return_duplicated_values([3,3,4,4,2,2,7,7,1,2,3,3,4,1,5,6,2]).sort).to eq([1,2,3,4,7])
+    expect(@array.return_duplicated_values([10,2,4,4,2,2,4,7,1,2,3,8,10,1,5,6,2]).sort).to eq([1,2,4,10])
+  end
 end
